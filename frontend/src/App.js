3acf274fbae501e7cb08e1019af5687a97d8d33e
1000039591 <stentiev07@gmail.com>
2026-05-22 01:02:29 +0600

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './containers/Home';
+import Signup from './containers/Signup';
+import Login from './containers/Login';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+
+import Layout from './hocs/Layout';
+
+const App = () => (
+    <Router>
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/reset-password' element={<ResetPassword />} />
+                <Route path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+                <Route path='/activate/:uid/:token' element={<Activate />} />
+            </Routes>
+        </Layout>
+    </Router>
+);
+
+export default App;
