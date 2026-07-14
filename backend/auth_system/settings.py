c5f9c40811a31383b6bedd6310291aec3435c2ec
YadernyiPonchik <shadowwolfs2000@example.com>
2026-07-14 11:26:32 +0600

    Fix DATABASES to actually use dj_database_url.config for DATABASE_URL

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -17,8 +17,8 @@ import dj_database_url
 
 
 
-import os
-from dotenv import load_dotenv
+#import os
+#from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,14 +113,11 @@ if os.getenv('USE_SQLITE', 'False').lower() == 'true':
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME', 'auth_system'),
-            'USER': os.getenv('DB_USER', 'postgres'),
-            'PASSWORD': os.getenv('DB_PASSWORD', 'default_pass'),
-            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
 
 # added for redis
